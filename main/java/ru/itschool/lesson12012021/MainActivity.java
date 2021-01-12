8f51b28197c7fe2e7edf1e2b01e38259adc0b7b1
paulmy <paulmul85@gmail.com>
2021-01-12 13:57:07 +0700

    update

--- main/java/ru/itschool/lesson12012021/MainActivity.java
+++ main/java/ru/itschool/lesson12012021/MainActivity.java
@@ -1,45 +1,104 @@
 package ru.itschool.lesson12012021;
 
-
-import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    Button btnStart, btnStop;
+    Button btn_start, btn_stop, btn_list;
+    SensorManager sensorManager;
+    Sensor lightsensor;
+    TextView textlight, textviewall;
+    List<Sensor> sensors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_start = findViewById(R.id.button_start);
+        btn_stop = findViewById(R.id.button_stop);
+        btn_list = findViewById(R.id.button_list);
+        textlight = findViewById(R.id.textlight);
+        textviewall = findViewById(R.id.textviewall);
 
-        btnStart = findViewById(R.id.button_start);
-        btnStop = findViewById(R.id.button_stop);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        lightsensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
-        // запуск службы
+        if (lightsensor == null) {
+            textlight.setText("Датчик освещения не обнаружен!");
+        } else {
+            textlight.append("Датчик освещения " + lightsensor.getName() + " доступен");
+            sensorManager.registerListener(LightSensorListener, lightsensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
 
-
-        btnStart.setOnClickListener(new View.OnClickListener() {
+        btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                // используем явный вызов службы
-                startService(
-                        new Intent(MainActivity.this, MyService.class));
+            public void onClick(View v) {
+
+                startService(new Intent(MainActivity.this, MyService.class));
             }
         });
 
-        // остановка службы
-        btnStop.setOnClickListener(new View.OnClickListener() {
+        btn_stop.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                stopService(
-                        new Intent(MainActivity.this, MyService.class));
+            public void onClick(View v) {
+                stopService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+
+        btn_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sensorManager.unregisterListener(LightSensorListener, lightsensor);
+                StringBuilder sb = new StringBuilder();
+                int i=0;
+                for (Sensor sensor : sensors){
+                    i++;
+                    sb.append(i+" name = ").append(sensor.getName())
+                            .append(", type = ").append(sensor.getType())
+                            .append("\nvendor = ").append(sensor.getVendor())
+                            .append(", version = ").append(sensor.getVersion())
+                            .append("\nmax = ").append(sensor.getMaximumRange())
+                            .append(", resolution = ").append(sensor.getResolution())
+                            .append("\n=============================================\n");
+                }
+                textviewall.setText(sb);
             }
         });
 
     }
+
+    private final SensorEventListener LightSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+                if (event.values[0] > 100) {
+                    startService(new Intent(MainActivity.this, MyService.class));
+                    textlight.setText("Light \n" + event.values[0]);
+                } else {
+                    stopService(new Intent(MainActivity.this, MyService.class));
+                    textlight.setText("Light \n" + event.values[0]);
+                }
+
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+
 }
--- main/java/ru/itschool/lesson12012021/MyService.java
+++ main/java/ru/itschool/lesson12012021/MyService.java
@@ -7,34 +7,33 @@ import android.os.IBinder;
 import android.widget.Toast;
 
 public class MyService extends Service {
-    private MediaPlayer mPlayer;
+
+    private MediaPlayer mediaPlayer;
+
+
     @Override
     public void onCreate() {
         super.onCreate();
-        Toast.makeText(this, "Служба создана",
-                Toast.LENGTH_SHORT).show();
-        mPlayer = MediaPlayer.create(this, R.raw.sting);
-        mPlayer.setLooping(false);
-    }
+        Toast.makeText(this,"Служба создана",Toast.LENGTH_SHORT).show();
+        mediaPlayer = MediaPlayer.create(this,R.raw.sting);
+        mediaPlayer.setLooping(false);
 
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "Служба запущена",
-                Toast.LENGTH_SHORT).show();
-        mPlayer.start();
+        Toast.makeText(this,"Служба запущена",Toast.LENGTH_SHORT).show();
+        mediaPlayer.start();
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
+
         super.onDestroy();
-        Toast.makeText(this, "Служба остановлена",
-                Toast.LENGTH_SHORT).show();
-        mPlayer.stop();
+        Toast.makeText(this,"Служба остановлена",Toast.LENGTH_SHORT).show();
+        mediaPlayer.stop();
     }
-
-
 
     @Override
     public IBinder onBind(Intent intent) {
