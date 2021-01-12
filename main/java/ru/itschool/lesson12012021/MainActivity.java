940bacef325efc79466f3e3c82d389f092e6a140
paulmy <paulmul85@gmail.com>
2021-01-12 12:20:02 +0700

    Add project

---
+++ main/java/ru/itschool/lesson12012021/MainActivity.java
@@ -0,0 +1,45 @@
+package ru.itschool.lesson12012021;
+
+
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnStart, btnStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.button_start);
+        btnStop = findViewById(R.id.button_stop);
+
+        // запуск службы
+
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // используем явный вызов службы
+                startService(
+                        new Intent(MainActivity.this, MyService.class));
+            }
+        });
+
+        // остановка службы
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(
+                        new Intent(MainActivity.this, MyService.class));
+            }
+        });
+
+    }
+}
---
+++ main/java/ru/itschool/lesson12012021/MyService.java
@@ -0,0 +1,44 @@
+package ru.itschool.lesson12012021;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class MyService extends Service {
+    private MediaPlayer mPlayer;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this, "Служба создана",
+                Toast.LENGTH_SHORT).show();
+        mPlayer = MediaPlayer.create(this, R.raw.sting);
+        mPlayer.setLooping(false);
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Служба запущена",
+                Toast.LENGTH_SHORT).show();
+        mPlayer.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Служба остановлена",
+                Toast.LENGTH_SHORT).show();
+        mPlayer.stop();
+    }
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
